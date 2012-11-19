ec323e47054ba0c47d33e3a802f235c9ab01edb1
Jérémy BENICHOU <jer.benichou@gmail.com>
2012-11-19 23:55:02 +0100

    Modification mineure sur la page Detail

--- SetC : SujetsetCorriges/DetailEpreuveViewController.m
+++ SetC : SujetsetCorriges/DetailEpreuveViewController.m
@@ -27,6 +27,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.title = @"Résumé";
     }
     return self;
 }
